3407bc343adb3782cbaa2f60878222a66443faf6
nicholas chikore <nickchikore@gmail.com>
2020-06-23 20:17:06 +0100

    first commit

---
+++ zimconnect.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using zimconnect.API.Models;
+
+namespace zimconnect.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
